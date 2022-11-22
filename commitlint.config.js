9394a34e46cdb74fc6601d83c8251a3abe914180
chenlu <chenlu0917boy@163.com>
2022-11-22 17:48:29 +0800

    fix: 增加husky

---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+/*
+ * @LastEditors: chenlu chenlu0917boy@163.com
+ * @Date: 2022-11-22 17:47:02
+ * @LastEditTime: 2022-11-22 17:47:33
+ * @FilePath: \demo-ts\commitlint.config.js
+ */
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+};
